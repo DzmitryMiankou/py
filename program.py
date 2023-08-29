708a23228d006e73bb184eb697dd9b3511822316
DzmitryMiankou <gmiankou@gmail.com>
2023-08-29 14:23:17 +0300

    цу

--- program.py
+++ program.py
@@ -1,20 +1,21 @@
 from random import randrange
 
-def multiplicationNumber(a, b):
+def multiplicationNumber(txt, a, b):
     result = a * b
+    if int(txt) == result:
+        print(f"+++++++++++++ :-) {a} * {b} = {result}")
+        return genericRandomNum(1, 10)
+    else:
+        return print(f"------------- :-( {a} * {b} = {result}")
+
+def inputDataNumber(a, b):
     txt = input(f"{a} * {b} = ")
     if txt.isdigit():
-        if int(txt) == result:
-            print(f":-) {a} * {b} = {result}")
-            return genericRandomNum(1, 10)
-        else:
-            return print(f":-( {a} * {b} = {result}")
+        multiplicationNumber(txt, a, b)
     else:
         return print('Вы ввели не число')
-
-
-        
+      
 def genericRandomNum(start, end):
-    return multiplicationNumber(randrange(start, end), randrange(start, end))
+    return inputDataNumber(randrange(start, end), randrange(start, end))
 
 genericRandomNum(1, 10)
