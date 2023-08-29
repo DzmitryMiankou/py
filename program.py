521af8f6fd2e8977ebabbc0cad66352827f740eb
DzmitryMiankou <gmiankou@gmail.com>
2023-08-29 20:54:28 +0300

    t

--- program.py
+++ program.py
@@ -1,6 +1,35 @@
 from random import randrange
 
-def multiplicationNumber(txt, a, b):
+class MathOperation:
+     def __init__(self, a, b):
+        self.a = randrange(a, b)
+        self.b = randrange(a, b)
+
+     def checkInput(self):
+        txt = input(f"{self.a} * {self.b} = ")
+        if txt.isdigit():
+            return self.multiplicationNumber(txt)
+        else:
+            return print('Вы ввели не число')
+        
+     def multiplicationNumber(self, txt): 
+        result = self.a * self.b
+        if int(txt) == result:
+            print(f"+++++++++++++ :-) {self.a} * {self.b} = {result}")
+            self.a = randrange(1, 10)
+            self.b = randrange(1, 10)
+            return self.checkInput()
+        else:
+            return print(f"------------- :-( {self.a} * {self.b} = {result}")
+        
+intervalNumber = MathOperation(1, 10)
+intervalNumber.checkInput()
+
+
+
+#function program
+
+'''def multiplicationNumber(txt, a, b):
     result = a * b  
     if int(txt) == result:
         print(f"+++++++++++++ :-) {a} * {b} = {result}")
@@ -18,4 +47,8 @@ def inputDataNumber(a, b):
 def genericRandomNum(start, end):
     return inputDataNumber(randrange(start, end), randrange(start, end))
 
-genericRandomNum(1, 10)
+genericRandomNum(1, 10)'''
+
+
+
+    
